5917b563b2180f88324e09cdec89275af14c9736
Anenqui <anenquipintadovenegas@gmail.com>
2025-05-20 14:40:23 -0600

    feat:Practica #1 FeathersJS

--- src/services/user/user.class.js
+++ src/services/user/user.class.js
@@ -2,31 +2,25 @@ export class UserService {
   constructor(options) {
     this.options = options || {};
   }
-
   async find(params) {
     const { name, lastName } = params.query;
 
     if (!name || !lastName) {
       throw new Error('Faltan parámetros: name y lastName');
     }
-
     return {
       message: `Hola ${name} ${lastName}`
     };
   }
-
   async get(id, params) {
     throw new Error('Método get no implementado');
   }
-
   async create(data, params) {
     throw new Error('Método create no implementado');
   }
-
   async patch(id, data, params) {
     throw new Error('Método patch no implementado');
   }
-
   async remove(id, params) {
     throw new Error('Método remove no implementado');
   }
